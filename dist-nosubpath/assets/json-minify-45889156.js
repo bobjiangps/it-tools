2493c21923ff0f26f18d9a89642bca604f48e938
Bo Jiang <bo.jiang@activenetwork.com>
2024-04-20 08:58:48 +0800

    debug files

---
+++ dist-nosubpath/assets/json-minify-45889156.js
@@ -0,0 +1,5 @@
+import{_ as a}from"./FormatTransformer.vue_vue_type_script_setup_true_lang-04e772da.js";import{l as r}from"./index-231c3e11.js";import{w as i}from"./defaults-4d6daddf.js";import{d as s,o as l,c as p}from"./index-87b6c522.js";import"./TextareaCopyable-76fe415c.js";import"./Copy-d002e731.js";import"./Scrollbar-d5a37766.js";const u=`{
+	"hello": [
+		"world"
+	]
+}`,N=s({__name:"json-minify",setup(m){const t=o=>i(()=>JSON.stringify(r.parse(o),null,0),""),e=[{validator:o=>o===""||r.parse(o),message:"Provided JSON is not valid."}];return(o,f)=>{const n=a;return l(),p(n,{"input-label":"Your raw JSON","input-default":u,"input-placeholder":"Paste your raw JSON here...","output-label":"Minified version of your JSON","output-language":"json","input-validation-rules":e,transformer:t})}}});export{N as default};
